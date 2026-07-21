bf48fbe90da876c5eddfb2c021e6e5288b8468e4
Thi Anh Thi Nguyen <athi.nguyen167@gmail.com>
2026-07-20 18:31:48 -0600

    feat: Implement navbar blur on scroll

--- script.js
+++ script.js
@@ -11,3 +11,29 @@ hamburgerBtn.addEventListener("click", () => {
   iconSpan.textContent = isMenuHidden ? "menu" : "close";
   document.body.style.overflow = isMenuHidden ? "" : "hidden";
 });
+
+window.addEventListener("scroll", () => {
+  const scrolled = window.pageYOffset;
+
+  // Navbar blur on scroll
+  const nav = document.querySelector("nav");
+  if (scrolled > 50) {
+    nav.classList.add(
+      "bg-background/80",
+      "backdrop-blur-xl",
+      "border-b",
+      "border-on-background/5",
+    );
+    nav.style.paddingTop = "20px";
+    nav.style.paddingBottom = "20px";
+  } else {
+    nav.classList.remove(
+      "bg-background/80",
+      "backdrop-blur-xl",
+      "border-b",
+      "border-on-background/5",
+    );
+    nav.style.paddingTop = "40px";
+    nav.style.paddingBottom = "40px";
+  }
+});
